ca2349ba83541d44f357e6b177620832ea9c5ec2
Igor Glagola <igor@glagola.ru>
2023-12-28 19:47:21 +0400

    Auto-commit at 2023-12-28 19:47:21

---
+++ commit_and_push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Change working directory to the script's folder
+cd "$(dirname "$0")"
+
+# Get the current date and time
+current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Check if there are any changes to commit
+if git diff --quiet; then
+  echo "No changes to commit."
+else
+  # Add all changes and commit
+  git add .
+  git commit -m "Auto-commit at ${current_datetime}"
+
+  # Push changes to the remote repository
+  git push origin master  # Change 'master' to your branch name if different
+fi
+
